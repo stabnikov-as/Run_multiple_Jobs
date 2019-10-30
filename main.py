8d1efe66fbc9d91b7636c90d5d26ac8fe66ef9ef
zahar <genavalera@yahoo.com>
2019-10-30 19:02:37 +0300

    first working version: starts tasks one by one

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import shlex
 #----------------------------------------------------------Constant-Values---------------------------------------------------------#
 Cts  = [1.0, 2.0]#, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0]
 Ats  = [1.0, 2.0]#, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0]
-Csss = [0.4, 0.6]#, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6]
+Csss = [0.4]#, 0.6]#, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6]
 a2  = 0.99#[]
 #----------------------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------------------Filenames-and-other-constants-------------------------------------------#
 cmd		= 'sbatch'
 job_fn	= 'tr_bl.pbs'#'matr.slrm'
 d_file  = 'd_uduct.in'
 cmd     = 'qsub' # 'sbatch'
+
 #------------------------------------------------------#
 def read_file(fn):
     f = open(fn, 'r')
@@ -30,7 +32,6 @@ def write_file(fn, ss):
     f.close()
 
 #------------------------------------------------------#
-
 def set_constants(path, ct, at, css, a2):
     in_fname = path + '/' + d_file
     menus = read_file(in_fname)
@@ -41,26 +42,31 @@ def set_constants(path, ct, at, css, a2):
             menus[i + 6]  ='   {}             ; C_psi    10.0                     14.5\n'.format(ct)
             menus[i + 10] ='   {}             ; a_l      0.45                     0.45\n'.format(a2)
     write_file(in_fname, menus)
+
 #------------------------------------------------------#
-def run_cmd(cmd):
+def run_cmd(cmd, cwd = None):
     command = shlex.split(cmd)
     p = subprocess.Popen(command,
-	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd = cwd)
     out, err = p.communicate()
     return (out, err)
 
 #------------------------------------------------------#
-def run_job(path, ids):
-    cmd_job = cmd + ' ' + path + '/' + job_fn
-    (o, e) = run_cmd(cmd_job)
+def run_job(path, pwd, ids):
+    path_to_job = pwd + path + '/'
+    print('ids = {}'.format(ids))
+    print('count_jobs = {}'.format(count_jobs(ids)))
+    while count_jobs(ids) > 2:
+        time.sleep(60)
+    cmd_job = cmd + ' ' + path_to_job + job_fn
+    (o, e) = run_cmd(cmd_job, cwd = path_to_job)
     id = o.split('.')[0]
-    ids.append(id)
     print('  Run job: {}'.format(id))
+    ids.append(id)
     return ids
 
 #------------------------------------------------------#
 def count_jobs(ids):
-    # wait for job
     num_jobs = 0
     s = 'q'
     (o, e) = run_cmd(s)
@@ -70,6 +76,9 @@ def count_jobs(ids):
     return num_jobs
 #------------------------------------------------------#
 def main():
+    cmd_path = 'pwd'
+    (o, e) = run_cmd(cmd_path)
+    pwd = o.split('\n')[0]+'/'
     ids = []
     for ct in Cts:
         path1 = 'Ct_=_' + str(ct)
@@ -84,7 +93,8 @@ def main():
                 s = 'cp -avr base {}'.format(path)
                 (o, e) = run_cmd(s)
                 set_constants(path, ct, at, css, a2)
-                ids = run_job(path, ids)
+                ids = run_job(path, pwd, ids)
+                print('started job with Ct = {}, At = {}, Css = {}'.format(ct, at, css))
 
 #------------------------------------------------------#
 if __name__ == '__main__':
